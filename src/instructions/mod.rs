e34d032428ecc7f0662d33b236c1359860491731
98prabowo <dimasprabowo98@icloud.com>
2025-10-04 10:48:49 +0700

    feat: create instruction enum

--- src/instructions/mod.rs
+++ src/instructions/mod.rs
@@ -5,3 +5,12 @@ pub mod update_nft_v1;
 pub use create_nft_v1::*;
 pub use transfer_nft_v1::*;
 pub use update_nft_v1::*;
+
+use borsh::{BorshDeserialize, BorshSerialize};
+
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+pub enum Instructions {
+    CreateNftV1(CreateNftV1InstructionData),
+    UpdateNftV1(UpdateNftV1InstructionData),
+    TransferNftV1,
+}
