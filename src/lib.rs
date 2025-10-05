bbbd022d0348c404d1726ccfdf73721dac7ac4a7
98prabowo <dimasprabowo98@icloud.com>
2025-10-05 23:00:01 +0700

    fix: move processor to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,14 @@
 mod instructions;
-mod processor;
 mod utils;
 
+use borsh::BorshDeserialize;
 use solana_program::{
     account_info::AccountInfo, declare_id, entrypoint, entrypoint::ProgramResult, pubkey::Pubkey,
+};
+
+use crate::{
+    instructions::{CreateNftV1, Instructions, TransferNftV1, UpdateNftV1},
+    utils::ProcessInstruction,
 };
 
 declare_id!("3KRCmsnNYQvjp1TZaha1riRmx5GGVt67yv2sxDsFXbRG");
@@ -11,9 +16,15 @@ declare_id!("3KRCmsnNYQvjp1TZaha1riRmx5GGVt67yv2sxDsFXbRG");
 entrypoint!(process_entrypoint);
 
 fn process_entrypoint(
-    program_id: &Pubkey,
+    _program_id: &Pubkey,
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
-    processor::process_entrypoint(program_id, accounts, instruction_data)
+    let instruction = Instructions::try_from_slice(instruction_data)?;
+
+    match instruction {
+        Instructions::CreateNftV1(data) => CreateNftV1::try_from((accounts, data))?.process(),
+        Instructions::UpdateNftV1(data) => UpdateNftV1::try_from((accounts, data))?.process(),
+        Instructions::TransferNftV1 => TransferNftV1::try_from(accounts)?.process(),
+    }
 }
